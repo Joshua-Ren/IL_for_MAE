395882a2f7223ab7ac6c21cb058b162fa5921da9
Joshua-Ren <renyi.joshua@gmail.com>
2022-02-15 17:57:19 -0800

    initial

---
+++ main_pretrain.py
@@ -0,0 +1,200 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# DeiT: https://github.com/facebookresearch/deit
+# BEiT: https://github.com/microsoft/unilm/tree/master/beit
+# --------------------------------------------------------
+import argparse
+import datetime
+import json
+import numpy as np
+import os
+import time
+from pathlib import Path
+
+from utils import *
+from data_loader_lmdb import ImageFolderLMDB
+
+import torch
+import torch.backends.cudnn as cudnn
+from torch.utils.tensorboard import SummaryWriter
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+import timm
+
+assert timm.__version__ == "0.3.2"  # version check
+import timm.optim.optim_factory as optim_factory
+
+import util.misc as misc
+from util.misc import NativeScalerWithGradNormCount as NativeScaler
+
+import models_mae
+
+from engine_pretrain import train_one_epoch
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('MAE pre-training', add_help=False)
+    parser.add_argument('--batch_size', default=64, type=int,
+                        help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
+    parser.add_argument('--epochs', default=400, type=int)
+    parser.add_argument('--accum_iter', default=1, type=int,
+                        help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
+
+    # Model parameters
+    parser.add_argument('--model', default='mae_vit_large_patch16', type=str, metavar='MODEL',
+                        help='Name of model to train')
+
+    parser.add_argument('--input_size', default=224, type=int,
+                        help='images input size')
+
+    parser.add_argument('--mask_ratio', default=0.75, type=float,
+                        help='Masking ratio (percentage of removed patches).')
+
+    parser.add_argument('--norm_pix_loss', action='store_true',
+                        help='Use (per-patch) normalized pixels as targets for computing loss')
+    parser.set_defaults(norm_pix_loss=False)
+
+    # Optimizer parameters
+    parser.add_argument('--weight_decay', type=float, default=0.05,
+                        help='weight decay (default: 0.05)')
+
+    parser.add_argument('--lr', type=float, default=None, metavar='LR',
+                        help='learning rate (absolute lr)')
+    parser.add_argument('--blr', type=float, default=1e-3, metavar='LR',
+                        help='base learning rate: absolute_lr = base_lr * total_batch_size / 256')
+    parser.add_argument('--min_lr', type=float, default=0., metavar='LR',
+                        help='lower lr bound for cyclic schedulers that hit 0')
+
+    parser.add_argument('--warmup_epochs', type=int, default=40, metavar='N',
+                        help='epochs to warmup LR')
+
+    # Dataset parameters
+    parser.add_argument('--data_path', default='/datasets01/imagenet_full_size/061417/', type=str,
+                        help='dataset path')
+
+    parser.add_argument('--run_name',default=None,type=str)
+    parser.add_argument('--proj_path',default='Interact_MAE', type=str)
+    
+    parser.add_argument('--output_dir', default='./output_dir',
+                        help='path where to save, empty for no saving')
+    parser.add_argument('--log_dir', default='./output_dir',
+                        help='path where to tensorboard log')
+    parser.add_argument('--device', default='cuda',
+                        help='device to use for training / testing')
+    parser.add_argument('--seed', default=0, type=int)
+    parser.add_argument('--resume', default='',
+                        help='resume from checkpoint')
+
+    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+                        help='start epoch')
+    parser.add_argument('--num_workers', default=10, type=int)
+    parser.add_argument('--pin_mem', action='store_true',
+                        help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+    parser.add_argument('--no_pin_mem', action='store_false', dest='pin_mem')
+    parser.set_defaults(pin_mem=True)
+
+    # distributed training parameters
+    parser.add_argument('--world_size', default=1, type=int,
+                        help='number of distributed processes')
+    parser.add_argument('--local_rank', default=-1, type=int)
+    parser.add_argument('--dist_on_itp', action='store_true')
+    parser.add_argument('--dist_url', default='env://',
+                        help='url used to set up distributed training')
+
+    return parser
+
+
+def main(args):
+    # ================= Prepare for distributed training =====
+    misc.init_distributed_mode(args)
+    device = torch.device(args.device)
+    # fix the seed for reproducibility
+    seed = args.seed + misc.get_rank()
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    cudnn.benchmark = True
+
+    # ================== Prepare for the dataloader ===============
+    # simple augmentation
+    transform_train = transforms.Compose([
+            transforms.RandomResizedCrop(args.input_size, scale=(0.2, 1.0), interpolation=3),  # 3 is bicubic
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    traindir = os.path.join('/home/sg955/rds/rds-nlp-cdt-VR7brx3H4V8/datasets/ImageNet/', 'train.lmdb')
+    dataset_train = datasets.ImageFolderLMDB(traindir, transform=transform_train)
+
+    if True:  # args.distributed:
+        num_tasks = misc.get_world_size()
+        global_rank = misc.get_rank()
+        sampler_train = torch.utils.data.DistributedSampler(
+            dataset_train, num_replicas=num_tasks, rank=global_rank, shuffle=True
+        )
+    else:
+        sampler_train = torch.utils.data.RandomSampler(dataset_train)
+
+    data_loader_train = torch.utils.data.DataLoader(
+        dataset_train, sampler=sampler_train,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        pin_memory=args.pin_mem,
+        drop_last=True,
+    )
+    
+    # =================== Initialize wandb ========================
+    if global_rank == 0:
+        run_name = wandb_init(proj_name=args.proj_name, run_name=args.run_name, config_args=args)
+        #base_folder = '/home/sg955/GitWS/IL_for_MAE/'
+        save_path = base_folder+'results/'+args.proj_name+'/'+args.model+'/'+run_name
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+    TRACK_TVX = wandb_gen_track_x(train_loader)
+    TRACK_TVX = TRACK_TVX.cuda() 
+    
+    # ================== Create the model: mae ==================
+    # define the model
+    model = models_mae.__dict__[args.model](norm_pix_loss=args.norm_pix_loss)
+    model.to(device)
+    model_without_ddp = model
+
+    eff_batch_size = args.batch_size * args.accum_iter * misc.get_world_size()
+    if args.lr is None:  # only base_lr is specified
+        args.lr = args.blr * eff_batch_size / 256
+
+    if args.distributed:
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
+        model_without_ddp = model.module
+    # following timm: set wd as 0 for bias and norm layers
+    param_groups = optim_factory.add_weight_decay(model_without_ddp, args.weight_decay)
+    optimizer = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.95))
+    loss_scaler = NativeScaler()
+
+    misc.load_model(args=args, model_without_ddp=model_without_ddp, optimizer=optimizer, loss_scaler=loss_scaler)
+
+    # ================= Train the model ===========================
+    for epoch in range(args.start_epoch, args.epochs):
+        if args.distributed:
+            data_loader_train.sampler.set_epoch(epoch)
+        train_one_epoch(model, data_loader_train,
+            optimizer, device, epoch, loss_scaler,
+            log_writer=log_writer,
+            args=args)
+        if epoch % 50 == 0 or epoch + 1 == args.epochs:
+            misc.save_model(
+                args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
+                loss_scaler=loss_scaler, epoch=epoch)
+        if misc.is_main_process():
+            _recon_validate(TRACK_TVX, model, table_key='last')
+            wandb.log({'epoch':g})
+            
+                
+if __name__ == '__main__':
+    args = get_args_parser()
+    args = args.parse_args()
+    main(args)
---
+++ utils.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 18 18:00:37 2021
+
+@author: YIREN
+"""
+import torch
+import torchvision
+import wandb
+import random
+import numpy as np
+import os
+import torch.distributed as dist
+import torchvision.transforms as T
+
+WANDB_track_figs = 6
+base_folder = '/home/sg955/rds/rds-nlp-cdt-VR7brx3H4V8/IL_for_MAE/'
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+def rnd_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+def checkpoint_save_interact(mae, g, save_path):
+    path = os.path.join(save_path,'checkpoint')
+    if not os.path.exists(path):
+        os.makedirs(path) 
+    file_name = 'encoder_ep'+str(g)+'.pt'
+    path = os.path.join(path, file_name)
+    torch.save(mae.state_dict(), path)
+    return path
+    
+def checkpoint_save_pretrain(encoder, g, save_path):
+    path = os.path.join(save_path,'checkpoint')
+    if not os.path.exists(path):
+        os.makedirs(path) 
+    file_name = 'encoder_ep'+str(g)+'.pt'
+    path = os.path.join(path, file_name)
+    torch.save(encoder.state_dict(), path)
+
+def ckp_converter(orig_model):
+    if list(orig_model.keys())[0][:6]=='module':
+        from collections import OrderedDict
+        new_model = OrderedDict()
+        for k, v in orig_model.items():
+            name = k[7:]
+            new_model[name]=v
+        return new_model
+    else:
+        return orig_model
+
+# ================== Functions about wandb ===================================
+def wandb_gen_track_x(train_loader,):
+    #for i,data in enumerate(train_loader):
+    #    x = data[0]['data']
+    for i, (x, y) in enumerate(train_loader):
+        break
+    track_tx = x[:WANDB_track_figs*2]
+    return track_tx
+
+def wandb_init(proj_name='test', run_name=None, config_args=None):
+    wandb.init(
+        project=proj_name,
+        config={})
+    if config_args is not None:
+        wandb.config.update(config_args)
+    if run_name is not None:
+        wandb.run.name=run_name
+        return run_name
+    else:
+        return wandb.run.name
+
+def wandb_record_results(results, epoch):
+  for key in results.keys():
+    wandb.log({key:results[key][-1]})
+  wandb.log({'epoch':epoch})
+
+def wandb_show16imgs(recon_imgs, origi_imgs, table_key='initial', ds_ratio=4):
+    '''
+        Show (n+n)*2 images in a table with table_key:
+        The input should be a tensor of (n+n,3,224,224), we down sample them
+            
+                From trainset           From valset
+        ------------------------------------------------
+        Recon      (n imgs)              (n imgs)
+        ------------------------------------------------
+        Origin     (n imgs)              (n imgs)
+        ------------------------------------------------
+    '''
+    recon_imgs = recon_imgs.clone()
+    origi_imgs = origi_imgs.clone()
+    assert recon_imgs.shape ==  origi_imgs.shape
+    n = int(recon_imgs.shape[0]/2)
+    _KEYS = ['train_','valid_']
+    #my_table = wandb.Table()
+    recon_list, origi_list, colunm_list = [],[],[]
+    for i in range(2):
+        for j in range(n):
+            idx = i*n+j
+            recon_list.append(wandb.Image(recon_imgs[idx,:,::ds_ratio,::ds_ratio]))
+            origi_list.append(wandb.Image(origi_imgs[idx,:,::ds_ratio,::ds_ratio]))
+            colunm_list.append(_KEYS[i]+str(j))
+            
+    show_data = [recon_list,origi_list]    
+    show_table = wandb.Table(data=show_data, columns=colunm_list)
+    wandb.log({table_key: show_table})  
+
+# ================= CIFAR_loader for single GPU ============================
+def get_cifar_loaders(args):
+    train_T=T.Compose([
+                        T.Resize(256),
+                        #T.RandomCrop(256),
+                        #T.RandomResizedCrop(256,scale=(0.05, 1.0)),
+                        T.RandomHorizontalFlip(),
+                        T.ToTensor(),
+                        T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                        ])
+    val_T =T.Compose([
+                        T.Resize(256),
+                        T.ToTensor(),
+                        T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                        ])
+    if args.dataset.lower()=='cifar10':
+        train_loader = torch.utils.data.DataLoader(
+                            torchvision.datasets.CIFAR10('./data', train=True, download=True, transform=train_T),
+                            batch_size=args.batch_size, shuffle=True, drop_last=True, num_workers=args.workers, pin_memory=True)
+        val_loader = torch.utils.data.DataLoader(
+                            torchvision.datasets.CIFAR10('./data', train=False, download=True, transform=val_T),
+                            batch_size=1000, shuffle=False, drop_last=True,pin_memory=True)    
+    elif args.dataset.lower()=='cifar100':
+        train_loader = torch.utils.data.DataLoader(
+                            torchvision.datasets.CIFAR100('./data', train=True, download=True, transform=train_T),
+                            batch_size=args.batch_size, shuffle=True, drop_last=True, num_workers=args.workers,pin_memory=True)
+        val_loader = torch.utils.data.DataLoader(
+                            torchvision.datasets.CIFAR100('./data', train=False, download=True, transform=val_T),
+                            batch_size=1000, shuffle=False, drop_last=True,pin_memory=True)
+    else:
+        print('args.dataset must be cifar10 or cifar100')
+    return train_loader, val_loader
