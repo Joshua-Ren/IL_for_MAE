933bdaca3e9f6b066d05e4edc3360de0e7287179
Joshua-Ren <renyi.joshua@gmail.com>
2022-02-15 18:40:23 -0800

    small bug

--- main_pretrain.py
+++ main_pretrain.py
@@ -180,15 +180,12 @@ def main(args):
     for epoch in range(args.start_epoch, args.epochs):
         if args.distributed:
             data_loader_train.sampler.set_epoch(epoch)
-        if misc.is_main_process():
-            _recon_validate(TRACK_TVX, model, table_key='last')
-            wandb.log({'epoch':epoch})
-        misc.save_model(
-            args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-            loss_scaler=loss_scaler, epoch=epoch)        
         train_one_epoch(model, data_loader_train,
             optimizer, device, epoch, loss_scaler,
             args=args)
+        if misc.is_main_process():
+            _recon_validate(TRACK_TVX, model, table_key='last')
+            wandb.log({'epoch':epoch})
         if epoch % 50 == 0 or epoch + 1 == args.epochs:
             misc.save_model(
                 args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
@@ -201,11 +198,10 @@ def _recon_validate(TRACK_TVX, model, table_key='initial'):
         For image reconstruction, feed TRACK_TVX to the mae model
         then show the reconstruction and original figure on W&B
     '''
-    loss, output, _ = model(TRACK_TVX)
+    _, output, _ = model(TRACK_TVX)
     recon_imgs = model.module.unpatchify(output)
     origi_imgs = TRACK_TVX
     wandb_show16imgs(recon_imgs, origi_imgs, table_key=table_key, ds_ratio=1)
-          
                 
 if __name__ == '__main__':
     args = get_args_parser()
