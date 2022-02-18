806d7137d211c7b677fa809c8b021557072e1fb9
Joshua-Ren <renyi.joshua@gmail.com>
2022-02-17 22:50:52 -0800

    inital

--- main_pretrain.py
+++ main_pretrain.py
@@ -186,7 +186,7 @@ def main(args):
         if misc.is_main_process():
             _recon_validate(TRACK_TVX, model, table_key='last')
             wandb.log({'epoch':epoch})
-        if epoch % 40 == 0 or epoch + 1 == args.epochs:
+        if epoch % 50 == 0 or epoch + 1 == args.epochs:
             misc.save_model(
                 args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
                 loss_scaler=loss_scaler, epoch=epoch)
