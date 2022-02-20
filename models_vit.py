744bead75ec3cb8b1f051b94a84f1e0a0ed060a5
Joshua-Ren <renyi.joshua@gmail.com>
2022-02-19 16:38:32 -0800

    inital

--- models_vit.py
+++ models_vit.py
@@ -55,8 +55,8 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
             cls_outcome = tmp2_x[:,0]
             return cls_outcome, word_outcome
         elif self.global_pool:
-            x = x[:, 1:, :].mean(dim=1)  # global pool without cls token
-            outcome = self.fc_norm(x)
+            #x = x[:, 1:, :].mean(dim=1)  # global pool without cls token
+            #outcome = self.fc_norm(x)
             return x#outcome
         else:
             x = self.norm(x)
