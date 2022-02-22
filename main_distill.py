41dfd730192c7cf6ccf4f089361404fdc6e13f28
Joshua-Ren <renyi.joshua@gmail.com>
2022-02-21 18:35:19 -0800

    add distill lp

--- main_distill.py
+++ main_distill.py
@@ -242,11 +242,11 @@ def main(args):
     )
 
     LP_data_loader_val = torch.utils.data.DataLoader(
-        LP_dataset_val, sampler=LP_sampler_val,
+        LP_dataset_val, shuffle=False,
         batch_size=args.batch_size,
         num_workers=args.num_workers,
         pin_memory=args.pin_mem,
-        drop_last=True
+        drop_last=True,
     )
 
     # =================== Initialize wandb ========================
