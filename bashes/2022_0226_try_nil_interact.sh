bfab6ed02a7ddc3463fe42484f20336edac00053
Joshua-Ren <renyi.joshua@gmail.com>
2022-02-26 12:40:34 -0800

    inital

--- bashes/2022_0226_try_nil_interact.sh
+++ bashes/2022_0226_try_nil_interact.sh
@@ -20,5 +20,5 @@ source /home/sg955/egg-env/bin/activate
 cd /home/sg955/GitWS/IL_for_MAE/
 
 python -m torch.distributed.launch --nproc_per_node=4 --master_port 1086 main_interact.py \
---run_name int400_dis50_int95 --batch_size 384 --en_epochs 90 --de_epochs 5 \
+--run_name int400_dis50_int95 --batch_size 384 --epochs 90 --de_epochs 5 \
 --en_ckp Distill-IN1K/vit_base_patch16/cos_distill_novalid/checkpoint-49.pth
