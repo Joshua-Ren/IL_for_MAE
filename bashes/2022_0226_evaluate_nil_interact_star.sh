08f4ea76a1aa6381ba8ea2d9b7db98d29219d9b1
Joshua-Ren <renyi.joshua@gmail.com>
2022-02-26 20:19:17 -0800

    inital

---
+++ bashes/2022_0226_evaluate_nil_interact_star.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -A NLP-CDT-SL2-GPU
+#SBATCH -p ampere
+#SBATCH --time=36:00:00
+#SBATCH --job-name=IN1K-finetune
+#SBATCH --output=./logs/test.txt
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:4
+
+export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=1
+
+. /etc/profile.d/modules.sh
+module purge
+module load rhel8/default-amp
+module load python/3.8 cuda/11.0 cudnn/8.0_cuda-11.1
+
+source /home/sg955/egg-env/bin/activate
+
+cd /home/sg955/GitWS/IL_for_MAE/
+
+srun python -m torch.distributed.launch --nproc_per_node=4 --master_port 1086 main_finetune.py --proj_name Finetune-IN1K \
+--run_name nil_int400_dis50_int95_start --batch_size 384 --dataset imagenet --epochs 100 \
+--finetune Interact_MAE/mae_vit_base_patch16_smallde/int400_dis50_int95/checkpoint-0.pth
--- main_interact.py
+++ main_interact.py
@@ -218,7 +218,7 @@ def main(args):
         if misc.is_main_process():
             _recon_validate(TRACK_TVX, model, table_key='latest')
             wandb.log({'epoch':epoch+args.de_epochs})
-        if epoch % 10 == 0 or epoch + 1 == args.de_epochs+args.epochs:
+        if epoch % 10 == 0 or epoch + 1 == args.epochs:
             misc.save_model(
                 args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
                 loss_scaler=loss_scaler, epoch=epoch)
