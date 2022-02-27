887ae80da8bd790ed7883c5ef8b79d45ef3ca11c
Joshua-Ren <renyi.joshua@gmail.com>
2022-02-26 21:23:57 -0800

    inital

--- bashes/2022_0226_evaluate_nil_interact_star.sh
+++ bashes/2022_0226_evaluate_nil_interact_star.sh
@@ -19,6 +19,6 @@ source /home/sg955/egg-env/bin/activate
 
 cd /home/sg955/GitWS/IL_for_MAE/
 
-srun python -m torch.distributed.launch --nproc_per_node=4 --master_port 1086 main_finetune.py --proj_name Finetune-CIFAR \
---run_name nil_int400_dis50_int95_start --batch_size 384 --dataset cifar100 --epochs 100 \
+srun python -m torch.distributed.launch --nproc_per_node=4 --master_port 1086 main_finetune.py --proj_name Finetune-IN1K \
+--run_name nil_int400_dis50_int95_start --batch_size 384 --dataset imagenet --epochs 100 \
 --finetune Interact_MAE/mae_vit_base_patch16_smallde/int400_dis50_int95/checkpoint-0.pth
