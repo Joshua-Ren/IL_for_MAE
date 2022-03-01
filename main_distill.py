a962aedc24a400a136ee2ffd9124e6275fab778e
Joshua-Ren <renyi.joshua@gmail.com>
2022-02-28 21:22:41 -0800

    mask distill

--- main_distill.py
+++ main_distill.py
@@ -45,7 +45,7 @@ def get_args_parser():
                         help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
 
     # Model parameters
-    parser.add_argument('--model', default='vit_base_patch16', type=str, metavar='MODEL',
+    parser.add_argument('--model', default='mae_vit_base_patch16_smallde', type=str, metavar='MODEL',
                         help='Name of model to train')
 
     parser.add_argument('--teachmodel', default='vit_base_patch16', type=str, metavar='MODEL',
