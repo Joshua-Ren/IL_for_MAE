0502a1371db276ba83161761210fb2df16e50744
Joshua-Ren <renyi.joshua@gmail.com>
2022-03-11 07:24:53 -0800

    inital

---
+++ bashes/2022_0311_ckpeval_s2_ep15inter_tlr.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -A NLP-CDT-SL2-GPU
+#SBATCH -p ampere
+#SBATCH --time=36:00:00
+#SBATCH --job-name=ckp_eval
+#SBATCH --output=./logs/test.txt
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:4
+
+export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=1
+
+. /etc/profile.d/modules.sh
+module purge
+module load rhel8/default-amp
+module load python/3.8 cuda/11.0 cudnn/8.0_cuda-11.1
+
+source /home/sg955/egg-env/bin/activate
+
+cd /home/sg955/GitWS/IL_for_MAE/
+
+srun python -m torch.distributed.launch --nproc_per_node=4 --master_port 1086 main_folder_finetune.py --proj_name Finetune-Folder \
+--run_name cosmask_t2s2_ep15_intertinylr --batch_size 384 --dataset cifar100 --epochs 50 --dis_or_interact interact \
+--ft_folder Interact_MAE/mae_vit_base_patch16_smallde/cosmask_t2s2_ep15_tinylr
